0d6b085a4af3b781f8103a823e60ac21b7229a12
sikderp-github <66568657+sikderp-github@users.noreply.github.com>
2020-08-22 22:05:05 +0600

    React practice

--- src/App.js
+++ src/App.js
@@ -3,20 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const nayoks = ['Anwar', 'Jafor', 'Alamgir', 'Bappi', 'Shuvo']
+  const [nayoks, setNayoks] = useState([])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setNayoks(data))
+  }, [])
+
+  // const nayoks = [{ name: 'Anwar', age: 45 }, { name: 'Jafor', age: 48 }, { name: 'Bappi', age: 40 }]
   const products = [
     { name: 'Photoshop', price: '$90.99' },
     { name: 'Illustrator', price: '$60.99' },
-    { name: 'PDF reader', price: '$6.99' }
   ]
+  const nayok1 = ['Razzak', 'Alamgir', 'Jashim']
   return (<div className="App" >
     <header className="App-header" >
       <h1>I am a react person.</h1>
+      <MovieCounter></MovieCounter>
+      {
+        nayoks.map(x => <Nayok name={x.name} key={x.id} age={x.age}></Nayok>)
+      }
       <Counter></Counter>
+      <Nayok name={nayok1[0]} age={'74'}></Nayok>
+      <Nayok name={nayok1[1]} age={'64'}></Nayok>
+      <Nayok name={nayok1[2]} age={''}></Nayok>
       <Users></Users>
       <ul>
         {
-          nayoks.map(nayok => <li>{nayok}</li>)
+          nayoks.map(nayok => <li>{nayok.name}</li>)
         }
         {
           products.map(product => <li>{product.name}</li>)
@@ -27,12 +41,43 @@ function App() {
       }
       <Person name='Munna' job="football"></Person>
       <Person name='Masum' job='Dorshak'></Person>
-      <Person></Person>
 
     </header>
   </div>
   );
 }
+function MovieCounter() {
+  const [count, setCount] = useState(0);
+  const handleClick = () => setCount(count + 1)
+  return (
+    <div>
+      <button onClick={handleClick}>Add Movies</button>
+      <h3>Number of movies: {count} </h3>
+      <MovieDisplay name={count + 5}></MovieDisplay>
+      <MovieDisplay name={count}></MovieDisplay>
+    </div>
+  )
+}
+
+function MovieDisplay(props) {
+  return (
+    <h4>Movies I acted: {props.name}</h4>
+  )
+}
+function Nayok(props) {
+  const nayokStyle = {
+    border: '2px solid purple',
+    margin: '10px'
+  }
+  return (<div style={nayokStyle}>
+    <h1>{props.name}</h1>
+    <h3>I acted in  5 movies in {props.age || 40} years.</h3>
+  </div>
+
+  )
+}
+
+
 function Users() {
   const [users, setUsers] = useState([]);
   useEffect(() => {
@@ -59,7 +104,7 @@ function Counter() {
   return (
     <div>
       <h1>Count: {count} </h1>
-      <button onMouseMove={() => setCount(count - 1)}>Decrease</button>
+      <button onMouseOut={() => setCount(count - 1)}>Decrease</button>
       <button onClick={handleIncrease}>Increase</button>
     </div>
   )
@@ -81,7 +126,6 @@ function Product(props) {
     height: '300px',
     float: 'left'
   }
-  console.log(props);
   return (
     <div style={productStyle}>
       <h3>{props.product.name} </h3>
